2e51221f94a59c1d9c256a50b755f28dd3b10358
Vadim <rfprod@users.noreply.github.com>
2020-10-17 23:06:59 +0300

    fix(webrtc): webrtc related fixes (#16)

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -80,7 +80,7 @@ export class AppChatComponent implements OnInit {
       iceCandidatePoolSize: 10,
     },
     roomId: 'T1vgzF95VyHhCewCbWG6',
-    senderId: getSenderId(),
+    senderId: getSenderId(), // TODO: this should be firebase user id (app should require firebase auth after local)
   };
 
   public readonly form = this.fb.group({
@@ -88,12 +88,16 @@ export class AppChatComponent implements OnInit {
     text: ['', Validators.compose([Validators.required])],
   });
 
-  public readonly roomRef$ = from(
-    this.firestore.collection('rooms').doc(this.webRtcConfig.roomId).get(),
-  ).pipe(
-    filter(roomSnapshot => roomSnapshot.exists),
-    first(),
+  private readonly roomRef$ = from(
+    this.firestore
+      .collection<TFirestoreRooms>('rooms')
+      .doc<IFirestoreRoom>(this.webRtcConfig.roomId)
+      .get(),
   );
+
+  private readonly iceCandidates = new BehaviorSubject<RTCIceCandidate[]>([]);
+
+  public readonly iceCandidates$ = this.iceCandidates.asObservable();
 
   private readonly peerConnection = new RTCPeerConnection(this.webRtcConfig.servers);
 
@@ -144,6 +148,18 @@ export class AppChatComponent implements OnInit {
     @Inject(NAVIGATOR) private readonly nav: TNavigator,
   ) {}
 
+  /**
+   * Sends chat message.
+   */
+  public sendChatMessage() {
+    if (this.form.valid) {
+      this.websocket.sockets.chat$.next(this.form.value);
+    }
+  }
+
+  /**
+   * Creates video call offer.
+   */
   public createOffer() {
     void this.roomRef$
       .pipe(
@@ -158,6 +174,10 @@ export class AppChatComponent implements OnInit {
       .subscribe();
   }
 
+  /**
+   * Accepts video call offer.
+   * @param peer RTC peer
+   */
   public acceptOffer(peer: IRtcPeer) {
     void from(
       this.peerConnection.setRemoteDescription(new RTCSessionDescription(peer.sdp ?? void 0)),
@@ -173,31 +193,35 @@ export class AppChatComponent implements OnInit {
       .subscribe();
   }
 
-  public sendChatMessage(): void {
-    if (this.form.valid) {
-      this.websocket.sockets.chat$.next(this.form.value);
-    }
-  }
-
+  /**
+   * Sends video room connection offer.
+   * @param room room snapshot
+   */
   // eslint-disable-next-line max-lines-per-function
-  public sendVideoRoomOffer(
+  private sendVideoRoomOffer(
     room: firebase.firestore.DocumentSnapshot<firebase.firestore.DocumentData>,
   ) {
     console.warn('sendVideoRoomOffer: room:', room);
     return this.videoRoomPeers$.pipe(
       first(),
       map(peers => {
-        const existingPeers = [...(peers.length > 1 + 1 + 1 ? [] : peers)].map(peer => {
-          const sdp = peer.sdp !== null ? JSON.stringify(peer.sdp) : null;
-          const processed: IRtcPeerDto = { ...peer, sdp };
-          return processed;
-        });
+        const existingPeers = [...(peers.length < 1 ? [] : peers)]
+          .filter(peer => {
+            return !(peer.type === 'offer' && peer.sender === this.webRtcConfig.senderId);
+          })
+          .map(peer => {
+            const sdp = peer.sdp !== null ? JSON.stringify(peer.sdp) : null;
+            const processed: IRtcPeerDto = { ...peer, sdp };
+            return processed;
+          });
         console.warn('sendVideoRoomOffer: existingPeers', existingPeers);
+
         const offerExists =
           typeof existingPeers.find(
             item => item.type === 'offer' && item.sender === this.webRtcConfig.senderId,
           ) !== 'undefined';
         console.warn('sendVideoRoomOffer: offerExists', offerExists);
+
         return { peers, existingPeers, offerExists };
       }),
       switchMap(({ peers, existingPeers, offerExists }) => {
@@ -242,19 +266,27 @@ export class AppChatComponent implements OnInit {
     );
   }
 
-  public sendVideoRoomAnswer(answer: RTCSessionDescriptionInit) {
+  /**
+   * Sends video room connection answer.
+   * @param answer connection answer
+   */
+  private sendVideoRoomAnswer(answer: RTCSessionDescriptionInit) {
     return this.videoRoomPeers$.pipe(
       first(),
       switchMap(peers => {
         console.warn('sendVideoRoomAnswer: peers', peers);
-        const existingPeers = [...(peers.length > 1 + 1 + 1 ? [] : peers)]
-          .filter(peer => !(peer.type === 'answer' && peer.sender === this.webRtcConfig.senderId))
+
+        const existingPeers = [...(peers.length < 1 ? [] : peers)]
+          .filter(peer => {
+            return !(peer.type === 'answer' && peer.sender === this.webRtcConfig.senderId);
+          })
           .map(peer => {
             const sdp = peer.sdp !== null ? JSON.stringify(peer.sdp) : null;
             const processed: IRtcPeerDto = { ...peer, sdp };
             return processed;
           });
         console.warn('sendVideoRoomAnswer: existingPeers', existingPeers);
+
         return from(
           this.firestore
             .collection<TFirestoreRooms>('rooms')
@@ -280,7 +312,21 @@ export class AppChatComponent implements OnInit {
     );
   }
 
-  public registerPeerConnectionListeners() {
+  /**
+   * Sets up local video stream and adds it to the peer connection.
+   * @param stream local media stream
+   */
+  private setupVideoStream(stream: MediaStream) {
+    this.localVideoStream.next(stream);
+    this.localVideoStream.value.getTracks().forEach(track => {
+      this.peerConnection.addTrack(track, this.localVideoStream.value);
+    });
+  }
+
+  /**
+   * Registers peer connection event listeners with respective action handlers.
+   */
+  private registerPeerConnectionListeners() {
     this.peerConnection.addEventListener('icegatheringstatechange', event => {
       console.warn(`ICE gathering state changed: ${this.peerConnection.iceGatheringState}`, event);
     });
@@ -298,7 +344,16 @@ export class AppChatComponent implements OnInit {
     });
 
     this.peerConnection.addEventListener('icecandidate', event => {
-      console.warn(`ICE Candidate: ${this.peerConnection.iceConnectionState}`, event);
+      console.warn(`ICE Candidate: ${this.peerConnection.iceConnectionState}`, event.candidate);
+      if (event.candidate !== null) {
+        const candidate = new RTCIceCandidate(event.candidate);
+        this.iceCandidates.next([...this.iceCandidates.value, candidate]);
+        /*
+        this.peerConnection.addIceCandidate(candidate).catch(error => {
+          console.error('peerConnection.addIceCandidate:', error);
+        });
+        */
+      }
     });
 
     this.peerConnection.addEventListener('track', event => {
@@ -310,47 +365,56 @@ export class AppChatComponent implements OnInit {
       });
       this.remoteVideoStream.next(remoteStream);
     });
-  }
 
-  public setupLocalStream(stream: MediaStream) {
-    this.localVideoStream.next(stream);
-    this.localVideoStream.value.getTracks().forEach(track => {
-      this.peerConnection.addTrack(track, this.localVideoStream.value);
+    this.peerConnection.addEventListener('datachannel', event => {
+      console.warn(`Datachannel, ${this.peerConnection.iceConnectionState}:`, event);
     });
   }
 
-  public ngOnInit(): void {
+  /**
+   * Gets media devices.
+   */
+  private getMediaDevices() {
+    if (typeof this.nav !== 'undefined') {
+      this.nav.mediaDevices
+        .enumerateDevices()
+        .then(devices => {
+          devices.forEach(item => {
+            this.mediaDevices.next([...this.mediaDevices.value, item]);
+          });
+        })
+        .catch(error => {
+          console.error('getMediaDevices:', error);
+        });
+    }
+  }
+
+  /**
+   * Connects user media.
+   */
+  private connectUserMedia() {
+    if (typeof this.nav !== 'undefined') {
+      this.nav.getUserMedia(
+        {
+          video: { width: { min: 320 }, height: { min: 240 } },
+          audio: true,
+        },
+        stream => {
+          this.setupVideoStream(stream);
+          this.registerPeerConnectionListeners();
+        },
+        error => {
+          console.error('connectMediaDevices:', error);
+        },
+      );
+    }
+  }
+
+  public ngOnInit() {
     void this.chatWs.pipe(untilDestroyed(this)).subscribe();
 
-    if (typeof this.nav !== 'undefined') {
-      if (Boolean(this.nav.mediaDevices) && Boolean(navigator.mediaDevices.enumerateDevices)) {
-        navigator.mediaDevices.enumerateDevices().then(
-          devices => {
-            devices.forEach(item => {
-              this.mediaDevices.next([...this.mediaDevices.value, item]);
-            });
-          },
-          error => {
-            console.warn('error', error);
-          },
-        );
-      }
+    this.getMediaDevices();
 
-      if (typeof this.nav.getUserMedia !== 'undefined') {
-        this.nav.getUserMedia(
-          {
-            video: { width: { min: 320 }, height: { min: 240 } },
-            audio: true,
-          },
-          stream => {
-            this.setupLocalStream(stream);
-            this.registerPeerConnectionListeners();
-          },
-          error => {
-            console.error('getUserMedia error:', error);
-          },
-        );
-      }
-    }
+    this.connectUserMedia();
   }
 }
